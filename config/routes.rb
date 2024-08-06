c10dcb60fce59f27990f6a770b6c963b41ff582b
kevin young <you.k2208@gmail.com>
2024-08-06 15:21:53 +0900

    added form and return to list button

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   #list all tasks
   get '/tasks', to: 'tasks#index', as: :tasks
 
+  #create a task
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
+
   #get one task
   get '/tasks/:id', to: 'tasks#show', as: :task
+
 end
